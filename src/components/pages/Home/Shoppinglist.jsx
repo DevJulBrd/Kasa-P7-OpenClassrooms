fa4795ac4fa82682fe382184a1dc95215e11fc5b
DevJulBrd <mjulien.bernard@icloud.com>
2023-03-29 23:30:53 +0200

    Try GET request

--- src/components/pages/Home/Shoppinglist.jsx
+++ src/components/pages/Home/Shoppinglist.jsx
@@ -1,21 +1,52 @@
 import '../../../styles/Shoppinglist.css'
-import { homeList } from '../../../data/Homelist'
-import HomeItem from './HomeItem'
+/*import HomeItem from './HomeItem'
+import { Link } from 'react-router-dom'*/
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
 
 
 function Shoppinglist() {
+
+    const { id } = useParams()
+    const [element, setElement] = useState(null)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        async function fetchElement() {
+            try {
+                const response = await fetch('https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/Front-End+V2/P9+React+1/logements.json', {
+                    method: 'HEAD',
+                    mode: 'no-cors'
+                })
+                if(!response.ok) {
+                    throw new Error(`Error ${response.status}`)
+                }
+                const data = await response.json()
+                setElement(data)
+            }
+            catch (error){
+                setError(error)
+            }
+        }
+        fetchElement()
+    }, [id])
+    
+console.log('element', element)
+console.log('error', error)
+
     return (
         <section className='shoppingList-container'>
-            <ul className='shoppingList-list'>
-                {homeList.map(({id, cover, title}) =>
-                    <article className='shoppingList-cart' key={id}>
+            <nav className='shoppingList-list'>
+                {/*data.map(({id, cover, title}) =>
+                    <Link to='/housing' key={id} className='shoppingList-cart' >
                         <HomeItem
                             cover={cover}
                             title={title}
                         />      
-                    </article>
-                )}
-            </ul>
+                    </Link>
+                )*/}
+            </nav>
         </section>
     )
 }
